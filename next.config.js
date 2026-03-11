5f75bec776c89f82c1e35af350641829419c167b
Pablo507 <45787130+Pablo507@users.noreply.github.com>
2026-03-11 07:16:25 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,14 +2,10 @@
 const nextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'plus.unsplash.com',
-      },
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'plus.unsplash.com' },
+      { protocol: 'https', hostname: 'vaaooxnolvjtpisufgbf.supabase.co' },
+      { protocol: 'https', hostname: '*.fal.ai' },
     ],
   },
   async headers() {
@@ -19,12 +15,11 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "img-src 'self' data: blob: https://images.unsplash.com https://plus.unsplash.com;",
+            value: "img-src 'self' data: blob: https://images.unsplash.com https://plus.unsplash.com https://vaaooxnolvjtpisufgbf.supabase.co https://*.fal.ai;",
           },
         ],
       },
     ];
   },
 };
-
 module.exports = nextConfig;
